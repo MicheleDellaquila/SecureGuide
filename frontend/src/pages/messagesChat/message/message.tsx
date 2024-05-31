50d2871eb40360439cd2566ba5ae323ae1efb9c7
MicheleDellaquila <micheledellaquila2003@gmail.com>
2024-05-31 09:03:06 +0200

    added logo if the message is from model

--- frontend/src/pages/messagesChat/message/message.tsx
+++ frontend/src/pages/messagesChat/message/message.tsx
@@ -3,6 +3,9 @@ import type { messageSender } from "@/types/types";
 import "./message.scss";
 import clsx from "clsx";
 
+// components
+import Logo from "@/components/ui/logo/logo";
+
 // Message props
 interface MessageProps extends HTMLProps<HTMLDivElement> {
   text: string;
@@ -11,7 +14,8 @@ interface MessageProps extends HTMLProps<HTMLDivElement> {
 
 const Message = ({ text, sender }: MessageProps) => {
   return (
-    <li className={clsx("Message", sender === "user" && "Message--user")}>
+    <li className={clsx("Message", sender === "user" ? "Message--user" : "Message--model")}>
+      {sender === "model" && <Logo width={36} height={36} className="Message__logo" />}
       <p className="Message__text">{text}</p>
     </li>
   );
