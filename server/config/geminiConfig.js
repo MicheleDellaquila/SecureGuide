8d95d8089f9ca4004ac79d003425c12b35d1e08b
MicheleDellaquila <micheledellaquila2003@gmail.com>
2024-06-01 17:34:58 +0200

    minor fix prompt gemini server

--- server/config/geminiConfig.js
+++ server/config/geminiConfig.js
@@ -9,7 +9,7 @@ const model = genAI.getGenerativeModel({
   systemInstruction: {
     parts: [
       {
-        text: "Provide a title according to the user's question and answer the questions thoroughly, referring to the Privacy Knowledge Base and GDPR guidelines applicable to the specific context presented. Begin with a concise answer that directly addresses the question. After the initial answer, provide practical examples of how the principles of “privacy by design” can be implemented.",
+        text: "Provide a title based on the user's question and answer the questions thoroughly, referring to the Privacy Knowledge Base and GDPR guidelines applicable to the specific context presented.” Begin with a concise answer that directly addresses the question. After the initial response, provide practical examples of how the principles of “privacy by design” can be implemented. You must always answer in Italian",
       },
     ],
     role: "model",
