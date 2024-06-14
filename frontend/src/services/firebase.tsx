c0d588d1a8bf2715fa2dc3bde9c9ec89cb3a7c65
MicheleDellaquila <micheledellaquila2003@gmail.com>
2024-06-14 19:34:21 +0200

    minor fix

--- frontend/src/services/firebase.tsx
+++ frontend/src/services/firebase.tsx
@@ -15,24 +15,31 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-let app;
-let firestore: Firestore;
-let analytics;
-let auth: Auth;
-let storage;
-let storageRef;
-let documentsRef;
+// Initialize Firebase
+const initializeFirebase = () => {
+  try {
+    const app = initializeApp(firebaseConfig);
+    const firestore = getFirestore(app);
+    const analytics = getAnalytics(app);
+    const auth = getAuth(app);
+    const storage = getStorage(app);
+    const storageRef = ref(storage);
+    const documentsRef = ref(storage, "documents");
 
-try {
-  app = initializeApp(firebaseConfig);
-  firestore = getFirestore(app);
-  analytics = getAnalytics(app);
-  auth = getAuth(app);
-  storage = getStorage(app);
-  storageRef = ref(storage);
-  documentsRef = ref(storage, "documents");
-} catch (error) {
-  console.error("Errore durante l'inizializzazione di Firebase:", error);
-}
+    return { app, firestore, analytics, auth, storage, storageRef, documentsRef };
+  } catch (error) {
+    console.error("Errore durante l'inizializzazione di Firebase:", error);
+    return {
+      app: null,
+      firestore: null,
+      analytics: null,
+      auth: null,
+      storage: null,
+      storageRef: null,
+      documentsRef: null,
+    };
+  }
+};
 
+const { app, firestore, analytics, auth, storage, storageRef, documentsRef } = initializeFirebase();
 export { app, analytics, auth, sendPasswordResetEmail, firestore, storage, storageRef, documentsRef, firebaseConfig };
