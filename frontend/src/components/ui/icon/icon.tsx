3921166aba8cda8b1e9f7b86fe3ea77200102a42
MicheleDellaquila <micheledellaquila2003@gmail.com>
2024-06-18 08:27:18 +0200

    added icon component

---
+++ frontend/src/components/ui/icon/icon.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import * as LucideIcons from "lucide-react";
+
+// const Icon = ({ name, size = 24, color = "black", ...rest }) => {
+//   // Verifica se l'icona richiesta esiste tra le icone disponibili
+//   if (!LucideIcons[name]) {
+//     console.error(`Icon '${name}' non trovata in Lucide Icons.`);
+//     return null;
+//   }
+
+//   const IconComponent = LucideIcons[name];
+
+//   return <IconComponent size={size} color={color} {...rest} />;
+// };
+
+// export default Icon;
